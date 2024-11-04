c26db0c074ef0617a14f0a6c2c2b1035e4da190c
Junior Hirwa <iamhirwejr@gmail.com>
2024-11-04 18:03:07 +0200

    passing data to results screen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/results_screen.dart';
 import 'package:quiz_app/start_screen.dart';
 import 'package:quiz_app/data/questions.dart';
 
@@ -23,10 +24,10 @@ class _QuizState extends State<Quiz> {
   void chooseAnswer(String answer) {
     selectedAnswers.add(answer);
 
-    if(selectedAnswers.length == questions.length) {
+    if (selectedAnswers.length == questions.length) {
       setState(() {
         selectedAnswers = [];
-        activeScreen = 'start-screen';
+        activeScreen = 'results-screen';
       });
     }
   }
@@ -37,6 +38,12 @@ class _QuizState extends State<Quiz> {
     if (activeScreen == 'questions-screen') {
       currentScreenWidget = QuestionsScreen(
         onSelectAnswer: chooseAnswer,
+      );
+    }
+
+    if (activeScreen == 'results-screen') {
+      currentScreenWidget = ResultsScreen(
+        choosenAnswers: selectedAnswers,
       );
     }
 
