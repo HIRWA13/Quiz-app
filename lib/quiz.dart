bcaa644935827ac4b7ad514df46ed18710e034f4
Junior Hirwa <iamhirwejr@gmail.com>
2024-11-04 17:37:19 +0200

    go to the start screen after answering all questioons

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/questions_screen.dart';
 import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -19,15 +20,24 @@ class _QuizState extends State<Quiz> {
     });
   }
 
-  void chooseAnswer (String answer) {
+  void chooseAnswer(String answer) {
     selectedAnswers.add(answer);
+
+    if(selectedAnswers.length == questions.length) {
+      setState(() {
+        selectedAnswers = [];
+        activeScreen = 'start-screen';
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     Widget currentScreenWidget = StartScreen(switchScreen);
     if (activeScreen == 'questions-screen') {
-      currentScreenWidget =  QuestionsScreen(onSelectAnswer: chooseAnswer,);
+      currentScreenWidget = QuestionsScreen(
+        onSelectAnswer: chooseAnswer,
+      );
     }
 
     return MaterialApp(
