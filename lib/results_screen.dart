99d622f65b045ee7be9bd9807bba8372488ada54
Junior Hirwa <iamhirwejr@gmail.com>
2024-11-06 22:13:37 +0200

    refactor: structure and render questions and and answers

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,22 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/questions_summary.dart';
 
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({super.key, required this.choosenAnswers});
+  const ResultsScreen({super.key, required this.chosenAnswers});
 
-  final List<String> choosenAnswers;
+  final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add(
+        {
+          'question_index': i,
+          'question': questions[i].question,
+          'correct_answer': questions[i].answers[0],
+          'user_answer': chosenAnswers[i],
+        },
+      );
+    }
+
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return  SizedBox(
+    var summaryData = getSummaryData();
+    var numOfQuestions = questions.length;
+    var numOfCorrectQuestions = summaryData.where((data) {
+      return data['user_answer'] == data['correct_answer'];
+    }).length;
+
+    return SizedBox(
       width: double.infinity,
       child: Container(
         margin: const EdgeInsets.all(40),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children:  [
-           const  Text('You have answered X questions of Y correctly'),
-           const Text('Answered questions container'),
-            TextButton(onPressed: () {}, child: const Text('Restart Quiz'),)
+          children: [
+            Text(
+              'You have answered $numOfCorrectQuestions out $numOfQuestions questions correctly!',
+              style: GoogleFonts.lato(
+                color: const Color(0xFFDAC2E7),
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            QuestionsSummary(summaryData),
+            const SizedBox(height: 30),
+            TextButton(
+              onPressed: () {},
+              child: const Text('Restart Quiz'),
+            )
           ],
         ),
       ),
